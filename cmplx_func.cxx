d194027f515c399938781b63075f0f62d9094f4d
Felix Matuschke <fmatuschke@pc07.pool.tp1.uni-duesseldorf.de>
2015-11-10 15:39:21 +0100

    finished

--- cmplx_func.cxx
+++ cmplx_func.cxx
@@ -1,13 +1,44 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+
+using namespace std;
+
+complex<double> calcz(double phi);
 
 int main(){
+  
+  int N = 1000;
 
   // Example how to declare a single complex variable:
-  complex<double> c;
+  complex<double> z[N];
+  complex<double> f[N];
 
   // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  // complex<double> d = complex<double>(1.0, 2.0); 
+  complex<double> ii = complex<double>(0.0, 1.0);
+  
+  double dphi = 2*M_PI/N;
+  double phi;
+  
+  for(int i = 0; i < N; i++)
+      z[i] = exp(i*dphi*ii);
+  
+  for(int i = 0; i < N; i++){
+    phi = i*dphi-M_PI;
+    f[i] = calcz(phi);
+  }
+  
+  for(int i = 0; i < N; i++){
+      //cout << i*dphi << "\t" << real(z[i]) << "\t" << imag(z[i]) << "\t" << abs(z[i]) << endl;
+    cout << i*dphi << "\t" << real(f[i]) << "\t" << imag(f[i]) << "\t" << abs(f[i]) << endl;
+  }
 
   return 0;
+}
+
+complex<double> calcz(double phi){
+  complex<double> ii = complex<double>(0.0, 1.0);
+  
+ return tan(phi*(1.+ii)); 
 }
--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <complex>
+#include <cmath>
+
+using namespace std;
 
 int main(){
 
   // Example how to declare a single complex variable:
-  complex<double> c;
-
+  complex<double> zn;
+  complex<double> znp1;
+  complex<double> z0;
+  int itmax = 400;
+  int iter;
+  double err;
+  
   // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
+  // complex<double> d = complex<double>(1.0, 2.0); 
+  
+  for(double re = -2; re <= 2; re+=0.1){
+      for(double im = -2; im <= 2; im+=0.1){
+	  z0 = complex<double>(re,im);
+	  iter = 0;
+	  zn = z0;
+	  err = 1;
+	  while((iter < itmax) & (err > 1e-8)){
+		iter++;
+		znp1 = zn-(pow(zn,3)-complex<double>(1,0))/(3.0*pow(zn,2));
+		err = abs(zn-znp1);
+		zn = znp1;
+	  }
+	  cout << re << "\t" << im << "\t" << iter << endl;
+      }
+  }
 
   return 0;
 }
